d89131e6fb75b5dcb1544a5566e227778a604463
Pooja03012005 <poojamadiwalar449@gmail.com>
2025-04-22 15:38:29 +0530

    changed

---
+++ Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name:Pooja");
+        System.out.println("Roll Number:74");
+        System.out.println("Marks:89");
+    }
+}
